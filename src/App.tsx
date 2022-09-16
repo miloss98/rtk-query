bcb8718d35a97cb3423bea2ff452a766f1c98014
stojanovic98m <stojanovic98m@gmail.com>
2022-09-16 16:59:40 +0200

    fetching almost works

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Counter } from "./features/counter/Counter";
+import { useFetchPeopleQuery } from "./features/faker-api/fakerApiSlice";
 import "./app.css";
 
 function App() {
+  const { data = [], isFetching } = useFetchPeopleQuery();
+  console.log(data);
+
   return (
     <div className="wrapper">
       <header>RTK Query practice</header>
       <section className="content">
-        <Counter />
+        {data?.map((person) => {
+          return (
+            <div key={person?.data?.id}>
+              {" "}
+              <p>{person?.data.firstname}</p>
+            </div>
+          );
+        })}
+
+        {/* <Counter /> */}
       </section>
     </div>
   );
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,16 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import { apiSlice } from "../features/faker-api/fakerApiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
   },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
