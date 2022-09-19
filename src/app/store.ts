f5bc8ab9e2a0b248f89604af5201257443e8bc15
stojanovic98m <stojanovic98m@gmail.com>
2022-09-19 15:39:22 +0200

    removed unused imports

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import { apiSlice } from "../features/faker-api/fakerApiSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
